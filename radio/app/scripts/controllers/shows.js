b6702ab76c69c1a7059c1335e2f6449217959939
Nathan Brizendine <nbrizend@gmail.com>
2014-01-11 11:35:24 +0700

    some changes

--- radio/app/scripts/controllers/shows.js
+++ radio/app/scripts/controllers/shows.js
@@ -2,6 +2,7 @@ angular.module('radioApp')
 
 .controller('ShowListController', function($scope, Shows){
 	Shows.query(function(data){
+	    var itemsPerRow = 4;
 	    $scope.shows = data;
 	    console.log($scope.shows);
 	});
