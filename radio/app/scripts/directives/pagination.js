317404539c1ba78889cd265be5da3d2511cfa524
Nathan Brizendine <nbrizend@gmail.com>
2014-01-26 12:35:44 +0700

    shows

--- radio/app/scripts/directives/pagination.js
+++ radio/app/scripts/directives/pagination.js
@@ -2,13 +2,13 @@ angular.module('radioApp')
 
 .directive('pagination', function() {
         return {
-                template: '<div id="pagination-container">' +
+                template: '<div class="row">' +
+                          '<div class="small-11 small-centered large-4 large-centered columns" style="position: relative; top: 10px;">' +
                           '<ul class="pagination" ng-hide="numPages() < 2">' + 
-                          '<li ng-class="{disabled: currentPage == 0}" ng-click="gotoPage(1)">«</li>' +
-                          '<li ng-class="{disabled: currentPage == 0}" ng-click="prevPage()"><</li>' +
-                              '<li ng-repeat="page in pages" ng-class="{active: currentPage+1 == page}"" ng-click="gotoPage(page)">{{page}}</li>' +
-                  '<li ng-class="{disabled: currentPage>= numPages() - 1}" ng-click="nextPage()">></li>' +
-                  '<li ng-class="{disabled: currentPage>= numPages() - 1}" ng-click="gotoPage(numPages())">»</li></ul>' +
+                          '<li class="arrow" ng-class="{unavailable: currentPage == 0}" ng-click="prevPage()"><a href="">&laquo;</a></li>' +
+                              '<li ng-repeat="page in pages" ng-class="{active: currentPage+1 == page}"" ng-click="gotoPage(page)"><a href="">{{page}}</a></li>' +
+                  '<li class="arrow" ng-class="{unavailable: currentPage>= numPages() - 1}" ng-click="nextPage()"><a href="">&raquo;</a></li>' +
+                  '</div>' +
                   '</div>',
                 replace: 'true',
                 restrict: 'EA',
