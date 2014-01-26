ab2915f3b3b3aa51757ded51c313ca9484e9fad5
Jordan Johnson <josamjohn@gmail.com>
2014-01-26 14:25:58 -0600

    Added Facebook widget, attempted getting about tabs to work (unsuccessful)

--- radio/app/scripts/controllers/shows.js
+++ radio/app/scripts/controllers/shows.js
@@ -18,3 +18,17 @@ angular.module('radioApp')
 	});
 });
 
+.controller('AboutController', ['$scope', 'Managers', 'Awards', '$routeParams', 'Page', function($scope, Managers, Awards, $routeParams, Page){
+	Page.setTitle('About');
+	var startTab = $routeParams.tab || "managers"
+	$scope.tabs = ['Managers', 'Contact', 'Mission Statement', 'Join Radio DePaul', 'Sponsor Radio DePaul', 'Awards and Recognition'];
+
+	//Set tab to routeParams tab
+	for(var i = 0; i < $scope.tabs.length; i++) {
+		if($scope.tabs[i].toLowerCase().replace(/ /g,"_") == startTab) {
+			$scope.tabs.active = ($scope.tabs[i]);
+		}
+	}
+	Managers.query(function(data){$scope.managers = data;});
+	Awards.query(function(data){$scope.awards = data;}); //401 error from the server
+}]);
